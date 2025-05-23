dc9c3a1726721572467f235c98d2ad1e90ed0858
vinola.rani <vinola.rani57@gmail.com>
2025-05-23 14:28:08 +0530

    titleChange

--- matrimony/src/app/pages/interested/interested.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-interested',
-  templateUrl: './interested.component.html'
-})
-export class InterestedComponent {
-  interestedProfiles = JSON.parse(localStorage.getItem('interested') || '[]');
-}
